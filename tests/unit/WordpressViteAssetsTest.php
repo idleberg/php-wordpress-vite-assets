44fcfbff6ca746e4b2584db6709d5db1b79356bf
Jan T. Sott <git@idleberg.com>
2022-04-01 17:07:12 +0200

    add tests

---
+++ tests/unit/WordpressViteAssetsTest.php
@@ -0,0 +1,46 @@
+<?php
+
+use Idleberg\WordpressViteAssets\WordpressViteAssets;
+
+class WordpressViteAssetsTest extends \Codeception\Test\Unit
+{
+    /**
+     * @var \UnitTester
+     */
+    protected $tester;
+
+    protected $basePath;
+    protected $viteAssets;
+
+    protected function _before()
+    {
+        $baseUrl = __DIR__ . "/../_data/";
+        $manifest = __DIR__ . "/../_data/manifest.json";
+
+        $this->basePath = realpath($baseUrl);
+        $this->viteAssets = new WordpressViteAssets($manifest, $baseUrl);
+    }
+
+    protected function _after()
+    {
+    }
+
+    // tests
+    public function testGetScriptTag()
+    {
+
+        $actual = $this->viteAssets->getScriptTag("demo.ts");
+        $expected = "<script type=\"module\" src=\"{$this->basePath}/assets/index.deadbeef.js\" crossorigin integrity=\"sha256-hK5PvH3PaGbMYq5EuedyA6F5uVkfoEwAznLNThffuZ8=\"></script>";
+
+        $this->assertEquals($actual, $expected);
+    }
+
+    public function testGetStyletags()
+    {
+        foreach ($this->viteAssets->getStyleTags("demo.ts") as $actual) {
+            $expected = "<link rel=\"stylesheet\" href=\"{$this->basePath}/assets/index.deadbeef.css\" crossorigin integrity=\"sha256-EEEKapOxnF8qZUxsx0ksgdBVnEB+8dXUJvH75TwCWvU=\" />";
+
+            $this->assertEquals($actual, $expected);
+        }
+    }
+}
