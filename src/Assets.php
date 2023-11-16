b5c342f8b52a875db734bb82746009712969df27
Jan T. Sott <git@idleberg.com>
2023-11-16 07:39:04 +0100

    format

--- src/Assets.php
+++ src/Assets.php
@@ -28,192 +28,192 @@ use Idleberg\ViteManifest\Manifest;
 
 class Assets
 {
-	private Manifest $vm;
-	private array $defaultOptions = [
-		"action" => null,
-		"crossorigin" => true,
-		"integrity" => true,
-		"priority" => 0
-	];
+    private Manifest $vm;
+    private array $defaultOptions = [
+        "action" => null,
+        "crossorigin" => true,
+        "integrity" => true,
+        "priority" => 0
+    ];
 
-	public function __construct(string $manifestFile, string $basePath, string $algorithm = "sha256")
-	{
-		// Let ViteManifest handle errors
-		$this->vm = new Manifest($manifestFile, $basePath, $algorithm);
-	}
+    public function __construct(string $manifestFile, string $basePath, string $algorithm = "sha256")
+    {
+        // Let ViteManifest handle errors
+        $this->vm = new Manifest($manifestFile, $basePath, $algorithm);
+    }
 
-	/**
-	 * Injects tags for entries specified in the manifest to the page header
-	 *
-	 * @param array|string $entrypoint
-	 * @param array $customOptions (optional)
-	 * @return void
-	 */
-	public function inject(array|string $entrypoint, array $customOptions = []): void
-	{
-		if (!function_exists('add_action')) {
-			throw new \Exception("WordPress function add_action() not found");
-		}
+    /**
+     * Injects tags for entries specified in the manifest to the page header
+     *
+     * @param array|string $entrypoint
+     * @param array $customOptions (optional)
+     * @return void
+     */
+    public function inject(array|string $entrypoint, array $customOptions = []): void
+    {
+        if (!function_exists('add_action')) {
+            throw new \Exception("WordPress function add_action() not found");
+        }
 
-		$options = $this->mergeOptions($customOptions);
-		["action" => $action, "priority" => $priority] = $options;
+        $options = $this->mergeOptions($customOptions);
+        ["action" => $action, "priority" => $priority] = $options;
 
-		if ($action === null || !is_string($action)) {
-			$action = is_admin() ? 'admin_head' : 'wp_head';
-		}
+        if ($action === null || !is_string($action)) {
+            $action = is_admin() ? 'admin_head' : 'wp_head';
+        }
 
-		if (!has_action($action)) {
-			throw new \Exception("The hook '$action' could not be found");
-		}
+        if (!has_action($action)) {
+            throw new \Exception("The hook '$action' could not be found");
+        }
 
-		$entries = is_array($entrypoint) ? $entrypoint : [$entrypoint];
+        $entries = is_array($entrypoint) ? $entrypoint : [$entrypoint];
 
-		add_action($action, function () use ($entries, $options) {
-			foreach ($entries as $entry) {
-				$tag = str_ends_with($entry, '.css')
-					? array_key_first($this->getStyleTags($entry, $options))
-					: $this->getScriptTag($entry, $options);
+        add_action($action, function () use ($entries, $options) {
+            foreach ($entries as $entry) {
+                $tag = str_ends_with($entry, '.css')
+                    ? array_key_first($this->getStyleTags($entry, $options))
+                    : $this->getScriptTag($entry, $options);
 
-				if ($tag) {
-					echo $tag . PHP_EOL;
-				}
-			}
-		}, $this->getPriority($priority, "scripts"), 1);
+                if ($tag) {
+                    echo $tag . PHP_EOL;
+                }
+            }
+        }, $this->getPriority($priority, "scripts"), 1);
 
-		add_action($action, function () use ($entries) {
-			foreach ($entries as $entry) {
-				foreach ($this->getPreloadTags($entry) as $preloadTag) {
-					echo $preloadTag . PHP_EOL;
-				}
-			}
-		}, $this->getPriority($priority, "preloads"), 1);
+        add_action($action, function () use ($entries) {
+            foreach ($entries as $entry) {
+                foreach ($this->getPreloadTags($entry) as $preloadTag) {
+                    echo $preloadTag . PHP_EOL;
+                }
+            }
+        }, $this->getPriority($priority, "preloads"), 1);
 
-		add_action($action, function () use ($entries, $options) {
-			foreach ($entries as $entry) {
-				foreach ($this->getStyleTags($entry, $options) as $styleTag) {
-					echo $styleTag . PHP_EOL;
-				}
-			}
-		}, $this->getPriority($priority, "styles"), 1);
-	}
+        add_action($action, function () use ($entries, $options) {
+            foreach ($entries as $entry) {
+                foreach ($this->getStyleTags($entry, $options) as $styleTag) {
+                    echo $styleTag . PHP_EOL;
+                }
+            }
+        }, $this->getPriority($priority, "styles"), 1);
+    }
 
-	/**
-	 * Returns the script tag for an entry in the manifest
-	 *
-	 * @param string $entrypoint
-	 * @param array $customOptions (optional)
-	 * @return string
-	 */
-	public function getScriptTag(string $entrypoint, array $customOptions = []): string
-	{
-		$options = $this->mergeOptions($customOptions);
-		$hash = $options["integrity"] ?? true;
-		$url = $this->vm->getEntrypoint($entrypoint, $hash);
+    /**
+     * Returns the script tag for an entry in the manifest
+     *
+     * @param string $entrypoint
+     * @param array $customOptions (optional)
+     * @return string
+     */
+    public function getScriptTag(string $entrypoint, array $customOptions = []): string
+    {
+        $options = $this->mergeOptions($customOptions);
+        $hash = $options["integrity"] ?? true;
+        $url = $this->vm->getEntrypoint($entrypoint, $hash);
 
-		if (!$url) {
-			return "";
-		}
+        if (!$url) {
+            return "";
+        }
 
-		$defaultAttributes = [
-			"type=\"module\"",
-			"src=\"{$url['url']}\""
-		];
+        $defaultAttributes = [
+            "type=\"module\"",
+            "src=\"{$url['url']}\""
+        ];
 
-		return "<script {$this->getAttributes($url, $defaultAttributes, $options)}></script>";
-	}
+        return "<script {$this->getAttributes($url, $defaultAttributes, $options)}></script>";
+    }
 
-	/**
-	 * Returns the style tags for an entry in the manifest
-	 *
-	 * @param string $entrypoint
-	 * @param array $customOptions (optional)
-	 * @return array
-	 */
-	public function getStyleTags(string $entrypoint, array $customOptions = []): array
-	{
-		$options = $this->mergeOptions($customOptions);
-		$hash = $options["integrity"] ?? true;
+    /**
+     * Returns the style tags for an entry in the manifest
+     *
+     * @param string $entrypoint
+     * @param array $customOptions (optional)
+     * @return array
+     */
+    public function getStyleTags(string $entrypoint, array $customOptions = []): array
+    {
+        $options = $this->mergeOptions($customOptions);
+        $hash = $options["integrity"] ?? true;
 
-		return array_map(function ($url) use ($options) {
-			$defaultAttributes = [
-				"rel=\"stylesheet\"",
-				"href=\"{$url['url']}\""
-			];
+        return array_map(function ($url) use ($options) {
+            $defaultAttributes = [
+                "rel=\"stylesheet\"",
+                "href=\"{$url['url']}\""
+            ];
 
-			return "<link {$this->getAttributes($url, $defaultAttributes, $options)} />";
-		}, $this->vm->getStyles($entrypoint, $hash));
-	}
+            return "<link {$this->getAttributes($url, $defaultAttributes, $options)} />";
+        }, $this->vm->getStyles($entrypoint, $hash));
+    }
 
-	/**
-	 * Returns the preload tags for an entry in the manifest
-	 *
-	 * @param string $entry
-	 * @return array
-	 */
-	public function getPreloadTags(string $entry): array
-	{
-		return array_map(function ($import) {
-			return "<link rel=\"modulepreload\" href=\"{$import['url']}\" />";
-		}, $this->vm->getImports($entry));
-	}
+    /**
+     * Returns the preload tags for an entry in the manifest
+     *
+     * @param string $entry
+     * @return array
+     */
+    public function getPreloadTags(string $entry): array
+    {
+        return array_map(function ($import) {
+            return "<link rel=\"modulepreload\" href=\"{$import['url']}\" />";
+        }, $this->vm->getImports($entry));
+    }
 
-	/**
-	 * Returns priority for an action
-	 *
-	 * @param array|int $priority
-	 * @param string $key
-	 * @return int
-	 */
-	private function getPriority(array|int $priority, string $key)
-	{
-		switch (true) {
-			case is_integer($priority):
-				return $priority;
+    /**
+     * Returns priority for an action
+     *
+     * @param array|int $priority
+     * @param string $key
+     * @return int
+     */
+    private function getPriority(array|int $priority, string $key)
+    {
+        switch (true) {
+            case is_integer($priority):
+                return $priority;
 
-			case is_array($priority) && is_integer($priority[$key]):
-				return $priority[$key];
+            case is_array($priority) && is_integer($priority[$key]):
+                return $priority[$key];
 
-			default:
-				return 0;
-		}
-	}
+            default:
+                return 0;
+        }
+    }
 
-	/**
-	 * Returns optional attributes for script or link tags
-	 *
-	 * @param array $url
-	 * @param array $attributes
-	 * @param array $customOptions
-	 * @return string
-	 */
-	private function getAttributes(array $url, array $attributes, array $customOptions)
-	{
-		["crossorigin" => $crossorigin, "integrity" => $integrity] = $this->mergeOptions($customOptions);
+    /**
+     * Returns optional attributes for script or link tags
+     *
+     * @param array $url
+     * @param array $attributes
+     * @param array $customOptions
+     * @return string
+     */
+    private function getAttributes(array $url, array $attributes, array $customOptions)
+    {
+        ["crossorigin" => $crossorigin, "integrity" => $integrity] = $this->mergeOptions($customOptions);
 
-		if ($crossorigin === true) {
-			$attributes[] = "crossorigin";
-		} elseif (in_array($crossorigin, ["anonymous", "use-credentials"])) {
-			$attributes[] = "crossorigin=\"{$crossorigin}\"";
-		}
+        if ($crossorigin === true) {
+            $attributes[] = "crossorigin";
+        } elseif (in_array($crossorigin, ["anonymous", "use-credentials"])) {
+            $attributes[] = "crossorigin=\"{$crossorigin}\"";
+        }
 
-		if ($integrity === true) {
-			$attributes[] = "integrity=\"{$url['hash']}\"";
-		}
+        if ($integrity === true) {
+            $attributes[] = "integrity=\"{$url['hash']}\"";
+        }
 
-		return join(" ", $attributes);
-	}
+        return join(" ", $attributes);
+    }
 
-	/**
-	 * Merges custom options with defaults
-	 *
-	 * @param array $options (optional)
-	 * @return array
-	 */
-	private function mergeOptions(array $options = [])
-	{
-		return array_merge(
-			$this->defaultOptions,
-			$options
-		);
-	}
+    /**
+     * Merges custom options with defaults
+     *
+     * @param array $options (optional)
+     * @return array
+     */
+    private function mergeOptions(array $options = [])
+    {
+        return array_merge(
+            $this->defaultOptions,
+            $options
+        );
+    }
 }
